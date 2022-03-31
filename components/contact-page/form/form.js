63ccc1a6b1231f595c14109e5ddbb31ae8db73ce
farhadham <farhadham2@gmail.com>
2022-03-31 20:01:52 +0300

    contact almost finished

--- components/contact-page/form/form.js
+++ components/contact-page/form/form.js
@@ -5,25 +5,43 @@ import { motion } from "framer-motion";
 function Form() {
   return (
     <div className={classes.container}>
-      <form>
-        <motion.input
-          className={classes.name}
-          type="text"
-          id="name"
-          placeholder="Your Name"
-          required
-          //   whileFocus={{ scale: 1.1 }}
-        />
-        <input
-          className={classes.email}
-          type="email"
-          id="email"
-          placeholder="Your Email"
-          required
-        />
+      <form action="https://formsubmit.co/farhadham2@gmail.com" method="POST">
+        <div className={classes.nameEmail}>
+          <motion.input
+            className={classes.name}
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Your Name"
+            required
+            whileFocus={{ backgroundColor: "#5A5758", color: "#ffffff" }}
+          />
+          <motion.input
+            className={classes.email}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your Email"
+            required
+            whileFocus={{ backgroundColor: "#5A5758", color: "#ffffff" }}
+          />
+        </div>
 
-        <textarea id="message" rows="4" placeholder="Your Message"></textarea>
-        <button type="submit">Submit</button>
+        <motion.textarea
+          id="message"
+          rows="4"
+          placeholder="Your Message"
+          name="text"
+          whileFocus={{ backgroundColor: "#5A5758", color: "#ffffff" }}
+        ></motion.textarea>
+        <motion.button
+          type="submit"
+          whileHover={{ y: -3, backgroundColor: "#E5143C" }}
+          whileTap={{ y: 3, backgroundColor: "#E5143C" }}
+          transition={{ duration: 0.1 }}
+        >
+          Submit
+        </motion.button>
       </form>
     </div>
   );
--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -2,17 +2,13 @@ import React, { useState } from "react";
 import Link from "next/link";
 import classes from "./navigation.module.scss";
 import { motion, AnimatePresence } from "framer-motion";
+import { Fragment } from "react";
 
 function Navigation() {
   const [navIsToggled, setNavIsToggled] = useState(false);
 
   return (
-    <motion.div
-      className={classes.navContainer}
-      animate={{ opacity: 1 }}
-      initial={{ opacity: 0 }}
-      transition={{ duration: 0.7 }}
-    >
+    <Fragment>
       <div
         className={classes.toggle}
         onClick={() => {
@@ -38,55 +34,72 @@ function Navigation() {
           ></motion.div>
         </div>
       </div>
-      <AnimatePresence>
-        {navIsToggled && (
-          <motion.div
-            className={classes.fakeBackground}
-            animate={{
-              scale: 30,
-            }}
-            transition={{ duration: 0.4 }}
-            exit={{ scale: 1 }}
-          ></motion.div>
-        )}
-      </AnimatePresence>
-      <div className={classes.listContainer}>
-        {navIsToggled && (
-          <ul className={classes.list}>
-            <Link href="./contact">
-              <motion.li
-                className={classes.listItem}
-                animate={{ opacity: 1 }}
-                initial={{ opacity: 0 }}
-                transition={{ duration: 0.4, delay: 0.2 }}
-              >
-                Contact
-              </motion.li>
-            </Link>
-            <Link href="./about">
-              <motion.li
-                className={classes.listItem}
-                animate={{ opacity: 1 }}
-                initial={{ opacity: 0 }}
-                transition={{ duration: 0.4, delay: 0.4 }}
-              >
-                About
-              </motion.li>
-            </Link>
-            <Link href="./portfolio">
-              <motion.li
-                className={classes.listItem}
-                animate={{ opacity: 1 }}
-                initial={{ opacity: 0 }}
-                transition={{ duration: 0.4, delay: 0.6 }}
-              >
-                Portfolio
-              </motion.li>
-            </Link>
-          </ul>
-        )}
-      </div>
-    </motion.div>
+      {navIsToggled && (
+        <motion.div
+          className={classes.navContainer}
+          transition={{ duration: 0.7 }}
+        >
+          <AnimatePresence>
+            (
+            <motion.div
+              className={classes.fakeBackground}
+              animate={{
+                scale: 30,
+              }}
+              transition={{ duration: 0.4 }}
+              exit={{ scale: 1 }}
+            ></motion.div>
+            )
+          </AnimatePresence>
+          <div className={classes.listContainer}>
+            {navIsToggled && (
+              <ul className={classes.list}>
+                <Link href="./contact">
+                  <motion.li
+                    className={classes.listItem}
+                    animate={{ opacity: 1 }}
+                    initial={{ opacity: 0 }}
+                    transition={{ duration: 0.4, delay: 0.2 }}
+                  >
+                    Contact
+                  </motion.li>
+                </Link>
+                <Link href="./about">
+                  <motion.li
+                    className={classes.listItem}
+                    animate={{ opacity: 1 }}
+                    initial={{ opacity: 0 }}
+                    transition={{ duration: 0.4, delay: 0.4 }}
+                  >
+                    About
+                  </motion.li>
+                </Link>
+                <Link href="./portfolio">
+                  <motion.li
+                    className={classes.listItem}
+                    animate={{ opacity: 1 }}
+                    initial={{ opacity: 0 }}
+                    transition={{ duration: 0.4, delay: 0.6 }}
+                  >
+                    Portfolio
+                  </motion.li>
+                </Link>
+                <Link href="./">
+                  <motion.li
+                    className={classes.listItem}
+                    animate={{ opacity: 1 }}
+                    initial={{ opacity: 0 }}
+                    transition={{ duration: 0.4, delay: 0.8 }}
+                  >
+                    Homepage
+                  </motion.li>
+                </Link>
+              </ul>
+            )}
+          </div>
+        </motion.div>
+      )}
+    </Fragment>
   );
 }
 
