6d019ad2e769a7333481101fc6886da759815b30
Farhad Faraji <68632547+farhadham@users.noreply.github.com>
2022-04-08 19:53:34 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300&family=Lora:ital,wght@1,400;1,500&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" type="image/x-icon" href="/images/myicon.ico"></link>
         </Head>
         <body>
           <Main />
